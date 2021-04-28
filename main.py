05274c11a9aa4b59ac654e688ba02549ae7b7bb4
Devwarlt <nadio.pontes@redes.unb.br>
2021-04-28 10:05:07 -0300

    Setting up local settings load model + dynamic CLI interaction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from utils.dialog import confirm_input_dialog
+from utils.input_dialogs import confirm_input_dialog
 from utils.logger import Log
+from utils.local_settings import LocalSettings
+from extensions.url import format_url, get_base_url
 from config import app_config
 from traceback import format_exc
 from sys import exit
@@ -17,20 +19,50 @@ try:
         f"{app_config['title']}\n\n"
         f"{app_config['description']}\n"
         f"{'~' * 50}")
-    __log.warning(
-        "Provide your credentials to access Rancher API.\n"
-        "If you don't have any API key yet, consider to visit "
-        f"URL:\n\t{app_config['rancher']['url']}/apikeys")
 
-    r_endpoint: str = confirm_input_dialog("Insert the Rancher endpoint:")
-    r_token: str = confirm_input_dialog("Insert the Bearer Token:")
-    r_token: list = r_token.split(':')
-    r_username: str = r_token[0]
-    r_password: str = r_token[1]
+    success, settings = LocalSettings.try_load()
+    if success and LocalSettings.try_parse(settings):
+        r_endpoint: str = app_config['rancher']['endpoint']
+        if not r_endpoint:
+            r_endpoint = confirm_input_dialog("Insert the Rancher endpoint:")
 
+        r_base_url: str = get_base_url(r_endpoint)
+        r_username: str = app_config['rancher']['username']
+        r_password: str = app_config['rancher']['password']
+        if not (r_username or r_password):
+            __log.warning(
+                "Provide your credentials to access Rancher API.\n"
+                "If you don't have any API key yet, consider to visit:\n"
+                f"\t{format_url(r_base_url, app_config['static']['api_keys'])}"
+            )
+
+            if not r_username:
+                r_username = confirm_input_dialog(
+                    "Insert your Rancher Access Key (username):")
+            if not r_password:
+                r_password = confirm_input_dialog(
+                    "Insert your Rancher Secret Key (password):")
+    else:
+        r_endpoint = confirm_input_dialog("Insert the Rancher endpoint:")
+        r_base_url: str = get_base_url(r_endpoint)
+
+        __log.warning(
+            "Provide your credentials to access Rancher API.\n"
+            "If you don't have any API key yet, consider to visit:\n"
+            f"\t{format_url(r_base_url, app_config['static']['api_keys'])}"
+        )
+
+        r_username = confirm_input_dialog(
+            "Insert your Rancher Access Key (username):")
+        r_password = confirm_input_dialog(
+            "Insert your Rancher Secret Key (password):")
+
+    app_config['rancher']['base_url'] = r_base_url
     app_config['rancher']['endpoint'] = r_endpoint
     app_config['rancher']['username'] = r_username
     app_config['rancher']['password'] = r_password
+
+    LocalSettings.save()
 except KeyboardInterrupt:
     __log.warning(f"{app_config['name']} is preparing to shutdown...")
 except Exception as error:
