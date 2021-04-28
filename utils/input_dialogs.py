becca8339b8cf6c6ba53574170645ea85899237c
Devwarlt <nadio.pontes@redes.unb.br>
2021-04-28 14:59:20 -0300

    Update input_dialogs.py

--- utils/input_dialogs.py
+++ utils/input_dialogs.py
@@ -2,6 +2,13 @@ from utils.logger import Log
 from extensions.str import is_null_or_whitespace
 
 
+def yes_or_no_input_dialog(question: str) -> bool:
+    log: Log = Log.get_singleton()
+    log.warning(f'{question} (Y/N)')
+    response: str = input()
+    return response and response.lower() == 'y'
+
+
 def confirm_input_dialog(question: str, nullable: bool = False) -> str:
     log: Log = Log.get_singleton()
     result: str = None
@@ -16,11 +23,7 @@ def confirm_input_dialog(question: str, nullable: bool = False) -> str:
 
         log.warning("Are you sure? (Y/N)")
         response: str = input()
-        if not response:
-            continue
-
-        response = response.lower()
-        if response != 'y':
+        if not response or response.lower() != 'y':
             continue
         break
     return result
